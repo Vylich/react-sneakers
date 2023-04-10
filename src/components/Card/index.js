4aee5ec57a3a81d62215a6a862fdceb24a5da598
Vylich <ilya.sidorenko.99@gmail.com>
2023-04-10 19:50:03 +0300

    bvbbcv

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -17,13 +17,15 @@ function Card({
 }) {
   const { isItemAdded } = React.useContext(AppContext);
   const [isFavorite, setIsFavorite] = React.useState(favorited);
+  const obj = { id, parentId: id, title, imageUrl, price }
+
 
   const onClickPlus = () => {
-    onPlus({ id, title, imageUrl, price });
+    onPlus(obj);
   };
 
   const onClickFavorite = () => {
-    onFavorite({ id, title, imageUrl, price });
+    onFavorite(obj);
     setIsFavorite(!isFavorite);
   };
 
@@ -36,7 +38,8 @@ function Card({
           height={250}
           viewBox="0 0 155 265"
           backgroundColor="#f3f3f3"
-          foregroundColor="#ecebeb">
+          foregroundColor="#ecebeb"
+        >
           <rect x="1" y="0" rx="10" ry="10" width="155" height="155" />
           <rect x="0" y="167" rx="5" ry="5" width="155" height="15" />
           <rect x="0" y="187" rx="5" ry="5" width="100" height="15" />
@@ -46,7 +49,12 @@ function Card({
       ) : (
         <>
           <div className={styles.favorite} onClick={onClickFavorite}>
-            <img src={isFavorite ? '/img/liked.svg' : '/img/unliked.svg'} alt="Unliked" />
+            {onFavorite && (
+              <img
+                src={isFavorite ? '/img/liked.svg' : '/img/unliked.svg'}
+                alt="Unliked"
+              />
+            )}
           </div>
           <img width="100%" height={135} src={imageUrl} alt="Sneakers" />
           <h5>{title}</h5>
@@ -55,12 +63,16 @@ function Card({
               <span>Цена:</span>
               <b>{price} руб.</b>
             </div>
-            <img
-              className={styles.plus}
-              onClick={onClickPlus}
-              src={isItemAdded(id) ? '/img/btn-checked.svg' : '/img/btn-plus.svg'}
-              alt="Plus"
-            />
+            {onPlus && (
+              <img
+                className={styles.plus}
+                onClick={onClickPlus}
+                src={
+                  isItemAdded(id) ? '/img/btn-checked.svg' : '/img/btn-plus.svg'
+                }
+                alt="Plus"
+              />
+            )}
           </div>
         </>
       )}
@@ -69,4 +81,3 @@ function Card({
 }
 
 export default Card;
-
--- src/components/Drawer/index.js
+++ src/components/Drawer/index.js
@@ -1,53 +1,72 @@
 import React from 'react';
 import axios from 'axios';
 
-import Info from './Info';
-import AppContext from '../context';
+import styles from './Drawer.module.scss'
+
+import Info from '../Info';
+import { useCart } from '../hooks/useCart';
 
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-function Drawer({ onClose, onRemove, items = [] }) {
-  const { cartItems, setCartItems } = React.useContext(AppContext);
+function Drawer({ onClose, onRemove, items = [], opened }) {
+  const { cartItems, setCartItems, totalPrice } = useCart();
   const [orderId, setOrderId] = React.useState(null);
   const [isOrderComplete, setIsOrderComplete] = React.useState(false);
   const [isLoading, setIsLoading] = React.useState(false);
 
+  const fee = (totalPrice * 0.05).toFixed(2);
+
   const onClickOrder = async () => {
     try {
       setIsLoading(true);
-      const { data } = await axios.post('https://642e15142b883abc6406adf3.mockapi.io/orders', {
-        items: cartItems,
-      });
+      const { data } = await axios.post(
+        'https://642e15142b883abc6406adf3.mockapi.io/orders',
+        {
+          items: cartItems,
+        }
+      );
       setOrderId(data.id);
       setIsOrderComplete(true);
       setCartItems([]);
 
       for (let i = 0; i < cartItems.length; i++) {
         const item = cartItems[i];
-        await axios.delete('https://642a18d5b11efeb759931224.mockapi.io/cart /' + item.id);
+        await axios.delete(
+          'https://642a18d5b11efeb759931224.mockapi.io/cart/' + item.id
+        );
         await delay(1000);
       }
     } catch (error) {
-      alert('Ошибка при создании заказа :(');
+      alert(error);
     }
     setIsLoading(false);
   };
 
   return (
-    <div className="overlay">
-      <div className="drawer">
+    <div className={`${styles.overlay} ${opened ? styles.overlayVisible : ''}` }>
+      <div className={styles.drawer}>
         <h2 className="d-flex justify-between mb-30">
-          Корзина <img onClick={onClose} className="cu-p" src="/img/btn-remove.svg" alt="Close" />
+          Корзина{' '}
+          <img
+            onClick={onClose}
+            className="cu-p"
+            src="/img/btn-remove.svg"
+            alt="Close"
+          />
         </h2>
 
         {items.length > 0 ? (
           <div className="d-flex flex-column flex">
-            <div className="items">
+            <div className="items flex">
               {items.map((obj) => (
-                <div key={obj.id} className="cartItem d-flex align-center mb-20">
+                <div
+                  key={obj.id}
+                  className="cartItem d-flex align-center mb-20"
+                >
                   <div
                     style={{ backgroundImage: `url(${obj.imageUrl})` }}
-                    className="cartItemImg"></div>
+                    className="cartItemImg"
+                  ></div>
 
                   <div className="mr-20 flex">
                     <p className="mb-5">{obj.title}</p>
@@ -67,15 +86,19 @@ function Drawer({ onClose, onRemove, items = [] }) {
                 <li>
                   <span>Итого:</span>
                   <div></div>
-                  <b>21 498 руб. </b>
+                  <b>{totalPrice} руб. </b>
                 </li>
                 <li>
                   <span>Налог 5%:</span>
                   <div></div>
-                  <b>1074 руб. </b>
+                  <b>{fee} руб. </b>
                 </li>
               </ul>
-              <button disabled={isLoading} onClick={onClickOrder} className="greenButton">
+              <button
+                disabled={isLoading}
+                onClick={onClickOrder}
+                className="greenButton"
+              >
                 Оформить заказ <img src="/img/arrow.svg" alt="Arrow" />
               </button>
             </div>
@@ -88,7 +111,11 @@ function Drawer({ onClose, onRemove, items = [] }) {
                 ? `Ваш заказ #${orderId} скоро будет передан курьерской доставке`
                 : 'Добавьте хотя бы одну пару кроссовок, чтобы сделать заказ.'
             }
-            image={isOrderComplete ? '/img/complete-order.jpg' : '/img/empty-cart.jpg'}
+            image={
+              isOrderComplete
+                ? '/img/complete-order.jpg'
+                : '/img/empty-cart.jpg'
+            }
           />
         )}
       </div>
